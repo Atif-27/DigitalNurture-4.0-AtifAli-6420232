ae4eccad96fa7a9b0f98afc85050946260a90884
Atif <atif276ali@gmail.com>
2025-07-06 00:23:40 +0530

    add: solution for week 3 spring assignment

---
+++ Week3/Exercise 1: Configuring a Basic Spring Application/LibraryManagement/src/main/java/com/library/MainApp.java
@@ -0,0 +1,19 @@
+package com.library;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.library.service.BookService;
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Load the Spring application context
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+        // Get the BookService bean
+        BookService bookService = (BookService)context.getBean("bookService");
+
+        // Test the service
+        bookService.addBook("Feluda Series");
+    }
+}
